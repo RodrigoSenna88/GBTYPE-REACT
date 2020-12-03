043eb5fb890518f7183b3d499004057848641006
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-12-03 18:17:20 -0300

    configurando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <h1>Hello World</h1>
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
